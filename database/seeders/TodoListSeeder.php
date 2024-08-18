aa0dcc6024582f2e18247f61077a3699a5fa4d9c
Y.inoue <kisin1010@gmail.com>
2024-08-18 18:15:54 +0900

    Todoアプリ作成開始、簡単はタスクviewの表示まで作成

--- database/seeders/TodoListSeeder.php
+++ database/seeders/TodoListSeeder.php
@@ -15,24 +15,19 @@ class TodoListSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('todo_lists')->insert(
+      DB::table('todo_lists')->insert(
         [
-            [
-                'name' => 'テスト1',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'テスト2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'テスト3',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+          [
+            'name' => 'テスト1',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+          [
+            'name' => 'テスト2',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
         ]
-        );
+      );
     }
 }
